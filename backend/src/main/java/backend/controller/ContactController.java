c27a903436812907d626b890137d8bd0d1f82685
Akash <ak54ash5@gmail.com>
2026-06-22 13:50:46 +0000

    Day 5 - Contact form backend integration

---
+++ backend/src/main/java/backend/controller/ContactController.java
@@ -0,0 +1,21 @@
+package backend.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ContactController {
+    @PostMapping("/contact")
+    public String submitForm(
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam String message) {
+
+        System.out.println("Name: " + name);
+
+        System.out.println("Email: " + email);
+
+        System.out.println("Message: " + message);
+
+        return "Form Submitted Successfully";
+    }
+}
